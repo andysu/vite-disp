e99ebff1d5cae7f808b20f1582d6d9518934ec77
Кислощаєв А В <adm@elsistema.com.ua>
2022-09-21 13:05:47 +0300

    add some from office

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -12,7 +12,7 @@ const count = ref(0)
   <h1>{{ msg }}</h1>
 
   <div class="card">
-    <button type="button" @wheel="count++" @click="count--">count is {{ count }}</button>
+    <button type="button" @wheel ="count++" @click="count--">count is {{ count }}</button>
     <p>
       Edit
       <code>components/HelloWorld!!.vue</code> to test HMR
@@ -20,9 +20,3 @@ const count = ref(0)
   </div>
 
 </template>
-
-<style scoped>
-.read-the-docs {
-  color: #888;
-}
-</style>
