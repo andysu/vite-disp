ea5bf86a31ff1153485a0353f265aafb793e0228
andy <adm@elsistema.com.ua>
2022-09-23 11:32:13 +0300

    add some from office

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,22 +1,19 @@
 <script setup>
-import { ref } from 'vue'
+import { ref } from "vue";
 
 defineProps({
-  msg: String
-})
+  msg: String,
+});
 
-const count = ref(0)
+const count = ref(0);
 </script>
 
 <template>
   <h1>{{ msg }}</h1>
-
   <div class="card">
-    <button type="button" @wheel ="count++" @click="count--">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld!!.vue</code> to test HMR
-    </p>
+    <button type="button" @wheel="count++" @click="count--">
+      count is {{ count }}
+    </button>
+    <p>Edit <code>components/HelloWorld!.vue</code> to test HMR</p>
   </div>
-
 </template>
